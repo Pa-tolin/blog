b915786198292bd4358a47aa397706da426b0288
Pa-tolin <contacto.infonet@gmail.com>
2022-04-26 11:48:46 -0400

    mejoras

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order('created_at DESC')
   end
 
   def create
+    @post = Post.create(
+      title: params['title'],
+      content: params['content'],
+      image: params['image'])
+   
+    if @post.save
+      redirect_to root_path
+    end
   end
 end
+
